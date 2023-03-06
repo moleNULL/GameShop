ade416e8c11b6c5ebd1ae4def9bbf449b5edaed8
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-03-06 16:54:53 +0200

    Add Order View in MVC

---
+++ Website/MVC/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        public async Task<IActionResult> ShowAsync()
+        {
+            await Task.Delay(0);
+            return View();
+        }
+    }
+}
